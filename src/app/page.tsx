92b8d8a26995d6e9e3fa57ba67624791e3103146
Lea Franz <leafranz4@gmail.com>
2023-06-06 20:22:29 +0200

    info table as component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import YearLineChart from "./yearLineChart";
 import MonthLineChart from "./monthLineChart";
 import useCountriesData from "./useCountriesData";
 import MagnitudeDepthSwitch from "./magnitudeDepthSwitch";
+import MagInfoTable from "./magInfoTable";
 
 const initialState: FilterState = {
   month: 1,
@@ -46,7 +47,6 @@ const HomePage = () => {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          //width: 400,
           fontStyle: "italic",
           marginTop: 25,
           marginBottom: 25,
@@ -92,51 +92,7 @@ const HomePage = () => {
             they are expressed in negative numbers. The scale also has no upper
             limit.
           </p>
-          <table style={{ marginTop: 15, marginBottom: 10 }}>
-            <thead>
-              <tr>
-                <th>Magnitude</th>
-                <th>Earthquake Effects</th>
-                <th>Estimated Number Each Year</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>2.5 or less</td>
-                <td>Usually not felt, but can be recorded by seismograph.</td>
-                <td>Millions</td>
-              </tr>
-              <tr>
-                <td>2.5 to 5.4</td>
-                <td>Often felt, but only causes minor damage.</td>
-                <td>500,000</td>
-              </tr>
-              <tr>
-                <td>5.5 to 6.0</td>
-                <td>Slight damage to buildings and other structures.</td>
-                <td>350</td>
-              </tr>
-              <tr>
-                <td>6.1 to 6.9</td>
-                <td>May cause a lot of damage in very populated areas.</td>
-                <td>100</td>
-              </tr>
-              <tr>
-                <td>7.0 to 7.9</td>
-                <td>Major earthquake. Serious damage.</td>
-                <td>10-15</td>
-              </tr>
-              <tr>
-                <td>8.0 or greater</td>
-                <td>
-                  Great earthquake. Can totally destroy communities near the
-                  epicenter.
-                </td>
-                <td>One every year or two</td>
-              </tr>
-            </tbody>
-          </table>
-
+          <MagInfoTable />
           <p>
             (Source:{" "}
             <a href="https://www.mtu.edu/geo/community/seismology/learn/earthquake-measure/magnitude/">
